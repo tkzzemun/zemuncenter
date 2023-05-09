8245d1ede2d8046320073604e0f48109bb37f369
Evgeniya <evgeniya@ultimatemovers.net>
2023-05-09 10:39:06 +0200

    added smoothScroll. js

--- js/main.js
+++ js/main.js
@@ -1,98 +1,13 @@
 
-
 /* code is here */
 
-//gallery js
+/*import SmoothScroll from './smoothScroll.js'*/
 
-/*if (document.body.id === 'Homepage') {
+(function () {
 
-const cardContainer = document.getElementById("card-container");
-const cardCountElem = document.getElementById("card-count");
-const cardTotalElem = document.getElementById("card-total");
-const loader = document.getElementById("loader");*/
-
-//const cardLimit = 99;
-/*const cardLimit = 36;
-const cardIncrease = 9;
-const pageCount = Math.ceil(cardLimit / cardIncrease);
-let currentPage = 1;
-
-cardTotalElem.innerHTML = cardLimit;
-
-var throttleTimer;
-const throttle = (callback, time) => {
-  if (throttleTimer) return;
-
-  throttleTimer = true;
-
-  setTimeout(() => {
-    callback();
-    throttleTimer = false;
-  }, time);
-};
-
-const getRandomColor = () => {
-  const h = Math.floor(Math.random() * 360);
-
-  return `hsl(${h}deg, 90%, 85%)`;
-};
-
-const createCard = (index) => {
-  const card = document.createElement("div");
-  card.className = "card";
-  card.innerHTML = index;
-  card.style.backgroundColor = getRandomColor();
-  cardContainer.appendChild(card);
-};
-
-const addCards = (pageIndex) => {
-  currentPage = pageIndex;
-
-  const startRange = (pageIndex - 1) * cardIncrease;
-  const endRange =
-    currentPage == pageCount ? cardLimit : pageIndex * cardIncrease;
-
-  cardCountElem.innerHTML = endRange;
-
-  for (let i = startRange + 1; i <= endRange; i++) {
-    createCard(i);
-  }
-};
-
-const handleInfiniteScroll = () => {
-  throttle(() => {
-    console.log('currentPage', currentPage)
-    console.log('document.body.offsetHeight', document.body.offsetHeight)
-    console.log('window.innerHeight + window.pageYOffset ', window.innerHeight + window.pageYOffset )
-    const endOfPage =
-      window.innerHeight + window.pageYOffset >= document.body.offsetHeight-300;
-
-    if (endOfPage) {
-      addCards(currentPage + 1);
-      console.log('its end of page')
-      console.log('currentPage',  currentPage )
-    }
-
-    if (currentPage === pageCount) {
-      console.log('remove scroll')
-      removeInfiniteScroll();
-     
-    }
-  }, 300);
-};
-
-const removeInfiniteScroll = () => {
-  loader.remove();
-  window.removeEventListener("scroll", handleInfiniteScroll);
-};
-
-window.onload = function () {
-  addCards(currentPage);
-};
-
-window.addEventListener("scroll", handleInfiniteScroll);
-}*/
-
+/*function init() {
+    new SmoothScroll({ target: document, speed: 40, smooth: 16 })
+  }*/
 
 //animation on scroll
 
@@ -273,5 +188,5 @@ if (document.querySelector('.reveal')) {
     }
   }
 
-
+})()
 
