3a0b78471de0d42510430256f487f47298139a39
Evgeniya <evgeniya@ultimatemovers.net>
2023-05-18 21:07:56 +0200

    screen height

--- js/main.js
+++ js/main.js
@@ -55,6 +55,12 @@ window.addEventListener('scroll', function() {
   }
 });
 
+//screen height
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 
 //animation on scroll
 
