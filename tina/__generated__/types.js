600540b027fb21f45e87260825113892bd0d18f2
Evgeniya <evgeniya@ultimatemovers.net>
2023-06-06 19:51:42 +0200

    test

--- tina/__generated__/types.js
+++ tina/__generated__/types.js
@@ -6,68 +6,77 @@ export function gql(strings, ...args) {
   return str;
 }
 export const PagesPartsFragmentDoc = gql`
-    fragment PagesParts on Pages {
-  ... on PagesHomepage {
-    layout
-    title
+  fragment PagesParts on Pages {
+    ... on PagesHomepage {
+      layout
+      title
+    }
   }
-}
-    `;
+`;
 export const PagesDocument = gql`
-    query pages($relativePath: String!) {
-  pages(relativePath: $relativePath) {
-    ... on Document {
-      _sys {
-        filename
-        basename
-        breadcrumbs
-        path
-        relativePath
-        extension
-      }
-      id
-    }
-    ...PagesParts
-  }
-}
-    ${PagesPartsFragmentDoc}`;
-export const PagesConnectionDocument = gql`
-    query pagesConnection($before: String, $after: String, $first: Float, $last: Float, $sort: String, $filter: PagesFilter) {
-  pagesConnection(
-    before: $before
-    after: $after
-    first: $first
-    last: $last
-    sort: $sort
-    filter: $filter
-  ) {
-    pageInfo {
-      hasPreviousPage
-      hasNextPage
-      startCursor
-      endCursor
-    }
-    totalCount
-    edges {
-      cursor
-      node {
-        ... on Document {
-          _sys {
-            filename
-            basename
-            breadcrumbs
-            path
-            relativePath
-            extension
-          }
-          id
+  query pages($relativePath: String!) {
+    pages(relativePath: $relativePath) {
+      ... on Document {
+        _sys {
+          filename
+          basename
+          breadcrumbs
+          path
+          relativePath
+          extension
         }
-        ...PagesParts
+        id
+      }
+      ...PagesParts
+    }
+  }
+  ${PagesPartsFragmentDoc}
+`;
+export const PagesConnectionDocument = gql`
+  query pagesConnection(
+    $before: String
+    $after: String
+    $first: Float
+    $last: Float
+    $sort: String
+    $filter: PagesFilter
+  ) {
+    pagesConnection(
+      before: $before
+      after: $after
+      first: $first
+      last: $last
+      sort: $sort
+      filter: $filter
+    ) {
+      pageInfo {
+        hasPreviousPage
+        hasNextPage
+        startCursor
+        endCursor
+      }
+      totalCount
+      edges {
+        cursor
+        node {
+          ... on Document {
+            _sys {
+              filename
+              basename
+              breadcrumbs
+              path
+              relativePath
+              extension
+            }
+            id
+          }
+          ...PagesParts
+        }
       }
     }
   }
-}
-    ${PagesPartsFragmentDoc}`;
+  ${PagesPartsFragmentDoc}
+`;
 export function getSdk(requester) {
   return {
     pages(variables, options) {
@@ -75,7 +84,7 @@ export function getSdk(requester) {
     },
     pagesConnection(variables, options) {
       return requester(PagesConnectionDocument, variables, options);
-    }
+    },
   };
 }
 import { createClient } from "tinacms/dist/client";
@@ -83,15 +92,18 @@ const generateRequester = (client) => {
   const requester = async (doc, vars, _options) => {
     const data = await client.request({
       query: doc,
-      variables: vars
+      variables: vars,
     });
     return { data: data?.data, query: doc, variables: vars || {} };
   };
   return requester;
 };
-export const ExperimentalGetTinaClient = () => getSdk(
-  generateRequester(createClient({ url: "http://localhost:4001/graphql", queries }))
-);
+export const ExperimentalGetTinaClient = () =>
+  getSdk(
+    generateRequester(
+      createClient({ url: "http://localhost:4001/graphql", queries })
+    )
+  );
 export const queries = (client) => {
   const requester = generateRequester(client);
   return getSdk(requester);
