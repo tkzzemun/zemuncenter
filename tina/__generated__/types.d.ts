85b88a215bab6bc38c09f3f412376a8b1bc1e669
Evgeniya <evgeniya@ultimatemovers.net>
2023-06-06 14:22:49 +0200

    check

--- tina/__generated__/types.d.ts
+++ tina/__generated__/types.d.ts
@@ -166,6 +166,7 @@ export type PagesHomepage = Node & Document & {
   __typename?: 'PagesHomepage';
   layout?: Maybe<Scalars['String']>;
   title?: Maybe<Scalars['String']>;
+  about?: Maybe<Scalars['String']>;
   id: Scalars['ID'];
   _sys: SystemInfo;
   _values: Scalars['JSON'];
@@ -183,6 +184,7 @@ export type StringFilter = {
 export type PagesHomepageFilter = {
   layout?: InputMaybe<StringFilter>;
   title?: InputMaybe<StringFilter>;
+  about?: InputMaybe<StringFilter>;
 };
 
 export type PagesFilter = {
@@ -263,20 +265,21 @@ export type DocumentMutation = {
 export type PagesHomepageMutation = {
   layout?: InputMaybe<Scalars['String']>;
   title?: InputMaybe<Scalars['String']>;
+  about?: InputMaybe<Scalars['String']>;
 };
 
 export type PagesMutation = {
   homepage?: InputMaybe<PagesHomepageMutation>;
 };
 
-export type PagesPartsFragment = { __typename?: 'PagesHomepage', layout?: string | null, title?: string | null };
+export type PagesPartsFragment = { __typename?: 'PagesHomepage', layout?: string | null, title?: string | null, about?: string | null };
 
 export type PagesQueryVariables = Exact<{
   relativePath: Scalars['String'];
 }>;
 
 
-export type PagesQuery = { __typename?: 'Query', pages: { __typename?: 'PagesHomepage', id: string, layout?: string | null, title?: string | null, _sys: { __typename?: 'SystemInfo', filename: string, basename: string, breadcrumbs: Array<string>, path: string, relativePath: string, extension: string } } };
+export type PagesQuery = { __typename?: 'Query', pages: { __typename?: 'PagesHomepage', id: string, layout?: string | null, title?: string | null, about?: string | null, _sys: { __typename?: 'SystemInfo', filename: string, basename: string, breadcrumbs: Array<string>, path: string, relativePath: string, extension: string } } };
 
 export type PagesConnectionQueryVariables = Exact<{
   before?: InputMaybe<Scalars['String']>;
@@ -288,13 +291,14 @@ export type PagesConnectionQueryVariables = Exact<{
 }>;
 
 
-export type PagesConnectionQuery = { __typename?: 'Query', pagesConnection: { __typename?: 'PagesConnection', totalCount: number, pageInfo: { __typename?: 'PageInfo', hasPreviousPage: boolean, hasNextPage: boolean, startCursor: string, endCursor: string }, edges?: Array<{ __typename?: 'PagesConnectionEdges', cursor: string, node?: { __typename?: 'PagesHomepage', id: string, layout?: string | null, title?: string | null, _sys: { __typename?: 'SystemInfo', filename: string, basename: string, breadcrumbs: Array<string>, path: string, relativePath: string, extension: string } } | null } | null> | null } };
+export type PagesConnectionQuery = { __typename?: 'Query', pagesConnection: { __typename?: 'PagesConnection', totalCount: number, pageInfo: { __typename?: 'PageInfo', hasPreviousPage: boolean, hasNextPage: boolean, startCursor: string, endCursor: string }, edges?: Array<{ __typename?: 'PagesConnectionEdges', cursor: string, node?: { __typename?: 'PagesHomepage', id: string, layout?: string | null, title?: string | null, about?: string | null, _sys: { __typename?: 'SystemInfo', filename: string, basename: string, breadcrumbs: Array<string>, path: string, relativePath: string, extension: string } } | null } | null> | null } };
 
 export const PagesPartsFragmentDoc = gql`
     fragment PagesParts on Pages {
   ... on PagesHomepage {
     layout
     title
+    about
   }
 }
     `;
